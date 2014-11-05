fb0bdb9f284d2421272323422514409881c45bc1
Chris Doskoch <chrisdoskoch@jenns-MacBook-Pro.local>
2014-11-05 01:00:12 -0800

    clear form js

--- js/change.js
+++ js/change.js
@@ -45,9 +45,10 @@ $(document).ready(function(){
 			submit.preventDefault();
 		} else {
 			$('#myModal').modal();
+			$('form').clearForm();
 			submit.preventDefault();
 
-$('form').clearForm();
+
 
 
 
