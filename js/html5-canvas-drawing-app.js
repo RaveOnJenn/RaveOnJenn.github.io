9246c5613f0e357877dccc7bf094c4dc0f9f6f7c
Chris Doskoch <chrisdoskoch@jenns-MacBook-Pro.local>
2014-11-16 22:00:38 -0800

    Change outline image

--- js/html5-canvas-drawing-app.js
+++ js/html5-canvas-drawing-app.js
@@ -94,31 +94,9 @@ function resourceLoaded()
 	
 	outlineImage.onload = function() { resourceLoaded(); 
 	};
-	//outlineImage.src = "images/RatTerrier.png";//
+	outlineImage.src = "images/RatTerrier.png";
 
-	//adding image feature
-	$('#image').change(function(){
-		if($('#image').val() == 'rat1') {
-			$('#canvas1').drawImage({
-				source: 'img/RatTerrier.png',
-				x: 365,
-				y: 260
-			});
-		} else if($('#image').val() == 'rat2') {
-			$('#canvas1').drawImage({
-				source: 'img/RatTerrier2.png',
-				x: 365,
-				y: 260
-			});
-		} else if($('#image').val() == 'clear') {
-			$('#canvas1').drawImage({
-				source: '',
-				x: 365,
-				y: 260
-			});
-		
-			});
-		} //end if
+	
 
 	// Add mouse events
 	// ----------------
