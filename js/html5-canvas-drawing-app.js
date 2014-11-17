4a77599059f964e4a622868622270b49b5efdd5e
Chris Doskoch <chrisdoskoch@jenns-MacBook-Pro.local>
2014-11-16 23:48:33 -0800

    trying to button function
    added .js, updated js and added end ‘else’ button

--- js/html5-canvas-drawing-app.js
+++ js/html5-canvas-drawing-app.js
@@ -1,41 +1,4 @@
 
-var canvas = document.querySelector('#paint');
-var ctx = canvas.getContext('2d');
- 
-var sketch = document.querySelector('#sketch');
-var sketch_style = getComputedStyle(sketch);
-canvas.width = parseInt(sketch_style.getPropertyValue('width'));
-canvas.height = parseInt(sketch_style.getPropertyValue('height'));
-
-var mouse = {x: 0, y: 0};
- 
-/* Mouse Capturing Work */
-canvas.addEventListener('mousemove', function(e) {
-  mouse.x = e.pageX - this.offsetLeft;
-  mouse.y = e.pageY - this.offsetTop;
-}, false);
-
-/* Drawing on Paint App */
-ctx.lineWidth = 5;
-ctx.lineJoin = 'round';
-ctx.lineCap = 'round';
-ctx.strokeStyle = 'blue';
- 
-canvas.addEventListener('mousedown', function(e) {
-    ctx.beginPath();
-    ctx.moveTo(mouse.x, mouse.y);
- 
-    canvas.addEventListener('mousemove', onPaint, false);
-}, false);
- 
-canvas.addEventListener('mouseup', function() {
-    canvas.removeEventListener('mousemove', onPaint, false);
-}, false);
- 
-var onPaint = function() {
-    ctx.lineTo(mouse.x, mouse.y);
-    ctx.stroke();
-};
 
 $(document).ready(function(){
 	$('#image').change(function(){
@@ -45,13 +8,19 @@ $(document).ready(function(){
 				x: 365,
 				y: 260
 			});
-		} else ($('#image').val() == 'rat2') {
+		} else if($('#image').val() == 'rat2') {
 			$('#canvas1').drawImage({
 				source: 'img/RatTerrier2.png',
 				x: 365,
 				y: 260
 			
 		
+			});
+			} else  {
+			$('#canvas1').drawImage({
+				source: '',
+				x: 365,
+				y: 260
 			});
 		} //end if
 	}); //end change
