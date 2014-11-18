1683102e26eef43aafff906ef89712ca27efea3f
Chris Doskoch <chrisdoskoch@jenns-MacBook-Pro.local>
2014-11-17 22:16:06 -0800

    change for button clear in draw

--- js/draw.js
+++ js/draw.js
@@ -1,5 +1,34 @@
 
-$('#clear').click(function(){
+$(document).ready(function(){
+    $('#image').change(function(){
+        if($('#image').val() == 'rat1') {
+            $('#canvas1').drawImage({
+                source: 'img/RatTerrier.png',
+                x: 365,
+                y: 260
+            });
+        } else if($('#image').val() == 'rat2') {
+            $('#canvas1').drawImage({
+                source: 'img/RatTerrier2.png',
+                x: 365,
+                y: 260
+            
+        
+            });
+            } else  {
+            $('#canvas1').drawImage({
+                source: '',
+                x: 365,
+                y: 260
+            });
+        } //end if
+    }); //end change
+
+
+    $('#clear').click(function(){
         $('#canvas1').clearCanvas();
     }); //end click-clear
 
+
+    
+    }); //end ready
