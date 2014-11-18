42de8159383df93f441ca3e46b224bf00c139fb5
Chris Doskoch <chrisdoskoch@jenns-MacBook-Pro.local>
2014-11-18 00:31:34 -0800

    Add text test

--- js/draw.js
+++ js/draw.js
@@ -1,5 +1,13 @@
+var canvas = document.getElementById('Canvas1');
+var ctx = canvas.getContext('2d');
+var x = 80;
+var y = 110;
 
-
+ctx.font = "bold 36px sans-serif";
+ctx.fillStyle = "blue";
+$('#entertext').click(function(){
+    ctx.fillText($("#words").val(), x, y);
+});
 
     $('#clear').click(function(){
         $('#canvas1').clearCanvas();
