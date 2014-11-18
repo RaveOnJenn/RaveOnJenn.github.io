7801bdc9194b45fe498b59f4b46a4d183e666f80
Chris Doskoch <chrisdoskoch@jenns-MacBook-Pro.local>
2014-11-18 00:23:19 -0800

    fix js for clear

--- js/draw.js
+++ js/draw.js
@@ -1,23 +1,4 @@
 
-$(document).ready(function(){
- $('#button1').click(function(){
-        $('#canvas1').drawEllipse({
-  fillStyle: '#FFFF7E',
-  x: 250, y: 100,
-  width: 200, height: 200
-});
-          .drawEllipse(({
-  fillStyle: '#C6E2FF',
-  x: 250, y: 150,
-  width: 200, height: 100
-});
-          .drawArc({
-            fillStyle: '#345678',
-            x: 300,
-            y: 150,
-            radius: 100
-        }); //end draw arc
-    }); // end circle click  
 
 
     $('#clear').click(function(){
@@ -26,4 +7,4 @@ $(document).ready(function(){
 
 
     
-    }); //end ready
+  
