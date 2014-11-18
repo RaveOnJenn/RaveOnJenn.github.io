9cb2e60c1e00b9d67816938c62dd9ffd89a125de
Chris Doskoch <chrisdoskoch@jenns-MacBook-Pro.local>
2014-11-17 21:44:40 -0800

    Adding function on draw page

--- js/draw.js
+++ js/draw.js
@@ -1,33 +1,33 @@
+function memetext(){
+            var line1 = $('#line1').val().toUpperCase();
+            var line2 = $('#line2').val().toUpperCase();
 
+            $('#canvas1').drawText({
+                fillStyle: '#fff',
+                strokeStyle: '#000',
+                strokeWidth: 3,
+                x: 365,
+                y: 50,
+                fontSize: '5em',
+                fontFamily: 'Impact, sans-serif',
+                text: line1
+            })
+            .drawText({
+                fillStyle: '#fff',
+                strokeStyle: '#000',
+                strokeWidth: 3,
+                x: 365,
+                y: 470,
+                fontSize: '5em',
+                fontFamily: 'Impact, sans-serif',
+                text: line2
+            }); //end draw text
+        } // end memetext
 
-$(document).ready(function(){
-	$('#image').change(function(){
-		if($('#image').val() == 'rat1') {
-			$('#canvas1').drawImage({
-				source: 'img/RatTerrier.png',
-				x: 365,
-				y: 260
-			});
-		} else if($('#image').val() == 'rat2') {
-			$('#canvas1').drawImage({
-				source: 'img/RatTerrier2.png',
-				x: 365,
-				y: 260
-			
-		
-			});
-			} else  {
-			$('#canvas1').drawImage({
-				source: '',
-				x: 365,
-				y: 260
-			});
-		} //end if
-	}); //end change
-
-
-	$('#clear').click(function(){
-		$('#canvas1').clearCanvas();
-	}); //end click-clear
-	
-	}); //end ready
+        //Make the magic happen and write to web page!
+        $('#canvas1').drawImage({
+            source: imgsrc,
+            x: 365,
+            y: 260,
+            load: memetext
+        }); //end draw image
