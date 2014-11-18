18267529687c472eaddd07da841aabfc86333659
Chris Doskoch <chrisdoskoch@jenns-MacBook-Pro.local>
2014-11-17 23:46:21 -0800

    update image for paint in draw

--- js/html5-canvas-paint-bucket.js
+++ js/html5-canvas-paint-bucket.js
@@ -1,27 +1,14 @@
-// Copyright 2010 William Malone (www.williammalone.com)
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// paintBucketApp is Copyright 2010 William Malone (www.williammalone.com)
+//with my additions JAshton 2014
 
-/*jslint browser: true */
-/*global G_vmlCanvasManager, $ */
 
 var paintBucketApp = (function () {
 
 	"use strict";
 
 	var context,
-		canvasWidth = 890,
-		canvasHeight = 420,
+		canvasWidth = 490,
+		canvasHeight = 220,
 		colorPurple = {
 			r: 203,
 			g: 53,
@@ -333,7 +320,7 @@ var paintBucketApp = (function () {
 				colorLayerData = context.getImageData(0, 0, canvasWidth, canvasHeight);
 				resourceLoaded();
 			};
-			outlineImage.src = "img/RatTerrier2.png";
+			outlineImage.src = "img/rat2.png";
 		};
 
 	return {
