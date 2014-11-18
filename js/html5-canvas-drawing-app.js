de6c688ae830b459e20420c0240afbd337a8a9a1
Chris Doskoch <chrisdoskoch@jenns-MacBook-Pro.local>
2014-11-17 21:22:04 -0800

    Revert b98cfc8..2ccbb60
    This rolls back to commit b98cfc81cb39fd11f52c68f03c1cd6a59f96cd9c.

---
+++ js/html5-canvas-drawing-app.js
@@ -0,0 +1,33 @@
+
+
+$(document).ready(function(){
+	$('#image').change(function(){
+		if($('#image').val() == 'rat1') {
+			$('#canvas1').drawImage({
+				source: 'img/RatTerrier.png',
+				x: 365,
+				y: 260
+			});
+		} else if($('#image').val() == 'rat2') {
+			$('#canvas1').drawImage({
+				source: 'img/RatTerrier2.png',
+				x: 365,
+				y: 260
+			
+		
+			});
+			} else  {
+			$('#canvas1').drawImage({
+				source: '',
+				x: 365,
+				y: 260
+			});
+		} //end if
+	}); //end change
+
+
+	$('#clear').click(function(){
+		$('#canvas1').clearCanvas();
+	}); //end click-clear
+	
+	}); //end ready
